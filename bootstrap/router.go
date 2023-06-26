9d082b57f4ac5a546cbbfc8a97be6ec0bd3b7168
WeiDa98 <weida@ones.ai>
2023-06-26 14:54:19 +0800

    modify: 修改退出时间

--- bootstrap/router.go
+++ bootstrap/router.go
@@ -53,19 +53,19 @@ func RunServer() {
 	}
 	log.Printf("Server Run: localhost%s \n", srv.Addr)
 
+	// 设置优雅退出
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
-	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	log.Println("Shutdown Server ...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// 等待中断信号以优雅地关闭服务器（设置 3 秒的超时时间）
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
